d036229b990a5dbc620ac0293e1c704e827517e0
Jiri Brejcha <jirka@jiribrejcha.net>
2023-05-25 00:13:42 +0100

    Use env variable to store API token

--- opt/wlanpi-grafana/scan/tografana.sh
+++ opt/wlanpi-grafana/scan/tografana.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-GRAFANA_TOKEN=$(cat /etc/grafana/grafana-token)
-
 while IFS= read -r line; do
   curl -X POST -H "Authorization: Bearer $GRAFANA_TOKEN" -d "$line" http://localhost:3000/api/live/push/$1
 done
