23efea97826cca28b75af907fe2f5e1bd344fcf1
Jiri Brejcha <jirka@jiribrejcha.net>
2023-05-24 23:44:01 +0100

    Save token to file instead of env var

--- opt/wlanpi-grafana/scan/tografana.sh
+++ opt/wlanpi-grafana/scan/tografana.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GRAFANA_TOKEN=$(cat /etc/grafana/grafana-token)
+
 while IFS= read -r line; do
   curl -X POST -H "Authorization: Bearer $GRAFANA_TOKEN" -d "$line" http://localhost:3000/api/live/push/$1
 done
