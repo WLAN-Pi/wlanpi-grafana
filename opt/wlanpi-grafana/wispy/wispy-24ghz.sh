49ec1256fe3e0ab0d554d66a4ae06f0458c29d4c
Jiri Brejcha <jirka@jiribrejcha.net>
2023-06-05 17:28:45 +0100

    Don't run scripts in the background

--- opt/wlanpi-grafana/wispy/wispy-24ghz.sh
+++ opt/wlanpi-grafana/wispy/wispy-24ghz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /opt/wlanpi-grafana/check-token.sh
 
-spectool_raw -r 0 2> /dev/null | ./spectool-client-parser.py > /dev/null &
+spectool_raw -r 0 2> /dev/null | ./spectool-client-parser.py > /dev/null
