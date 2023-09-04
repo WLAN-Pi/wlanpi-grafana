20c753f9dcec4bc253a5453628b6208272808262
Josh Schmelzle <josh@joshschmelzle.com>
2023-09-04 09:14:29 -0400

    handle 'float' object has no attribute 'replace'

--- install/opt/wlanpi-grafana/scan/iwscan.py
+++ install/opt/wlanpi-grafana/scan/iwscan.py
@@ -6,8 +6,11 @@ data = json.load(sys.stdin)
 
 for d in data:
     if 'ssid' in d:
-        d['ssid'] = d['ssid'].replace(" ", "\ ")
-        print(f'wlanpi,name={d["ssid"]}\ {d["bssid"]},ssid={d["ssid"]},bssid={d["bssid"]},channel={d["primary_channel"]},freq={d["freq"]} rssi={d["signal_dbm"]},channel={d["primary_channel"]},freq={d["freq"]}')
+        try:
+            d['ssid'] = d['ssid'].replace(" ", "\ ")
+            print(f'wlanpi,name={d["ssid"]}\ {d["bssid"]},ssid={d["ssid"]},bssid={d["bssid"]},channel={d["primary_channel"]},freq={d["freq"]} rssi={d["signal_dbm"]},channel={d["primary_channel"]},freq={d["freq"]}')
+        except AttributeError:
+            pass
     else:
         print(f'wlanpi,name={d["bssid"]},bssid={d["bssid"]},channel={d["primary_channel"]},freq={d["freq"]} rssi={d["signal_dbm"]},channel={d["primary_channel"]},freq={d["freq"]}')
 
