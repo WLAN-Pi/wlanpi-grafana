3d39dc1366467c6a183688b656f54c1f452afdc9
Jiri Brejcha <jirka@jiribrejcha.net>
2023-09-13 15:29:10 +0100

    Refactor password change

--- install/opt/wlanpi-grafana/check-token.sh
+++ install/opt/wlanpi-grafana/check-token.sh
@@ -23,11 +23,11 @@ if [ ${#GRAFANA_TOKEN} -lt 40 ]; then
 
     # Create wlanpi service account
     echo "Creating Grafana service account wlanpi"
-    SA_RESPONSE=$(curl -s --insecure -X POST https://admin:wlanpi@127.0.0.1/app/grafana/api/serviceaccounts -H "Accept: application/json" -H "Content-Type: application/json" -d '{"name": "wlanpi", "role": "Admin", "isDisabled": false}' -w " StatusCode:%{http_code}")
+    SA_RESPONSE=$(curl -s --insecure -X POST https://wlanpi:wlanpi@127.0.0.1/app/grafana/api/serviceaccounts -H "Accept: application/json" -H "Content-Type: application/json" -d '{"name": "wlanpi", "role": "Admin", "isDisabled": false}' -w " StatusCode:%{http_code}")
 
     # Generate API token for wlanpi service account. Service account must use name wlanpi and ID 2. Otherwise following command fails.
     echo "Generating Grafana API token for service account wlanpi"
-    NEW_GRAFANA_TOKEN=$(curl -s --insecure -X POST https://admin:wlanpi@127.0.0.1/app/grafana/api/serviceaccounts/2/tokens -H "Accept: application/json" -H "Content-Type: application/json" -d '{"name": "wlanpi", "role": "Admin"}' | jq -r '.key')
+    NEW_GRAFANA_TOKEN=$(curl -s --insecure -X POST https://wlanpi:wlanpi@127.0.0.1/app/grafana/api/serviceaccounts/2/tokens -H "Accept: application/json" -H "Content-Type: application/json" -d '{"name": "wlanpi", "role": "Admin"}' | jq -r '.key')
 
     if [ ${#NEW_GRAFANA_TOKEN} -gt 40 ]; then
         # Save API token to env variable
