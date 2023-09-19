5661e2bce69d631a2abf00c1351d9f857da8067a
Jiri Brejcha <jirka@jiribrejcha.net>
2023-09-19 23:14:44 +0100

    Fix path and URL

--- install/opt/wlanpi-grafana/health/tografana.sh
+++ install/opt/wlanpi-grafana/health/tografana.sh
@@ -3,5 +3,5 @@
 source /etc/environment
 
 while IFS= read -r line; do
-  curl -X POST -H "Authorization: Bearer $GRAFANA_TOKEN" -d "$line" http://localhost:3000/api/live/push/$1
+    curl --insecure -X POST -H "Authorization: Bearer $GRAFANA_TOKEN" -d "$line" https://localhost/app/grafana/api/live/push/$1
 done
