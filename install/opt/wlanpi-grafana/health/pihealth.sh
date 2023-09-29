0e34158100c395334018b667604753ab9f7038ea
Bryan Ward <57335384+bryanward-net@users.noreply.github.com>
2023-09-29 14:12:21 -0400

    Update pihealth.sh
    Changed functions for cpu_util, disk usage.
    Commented out check functions until they are needed

--- install/opt/wlanpi-grafana/health/pihealth.sh
+++ install/opt/wlanpi-grafana/health/pihealth.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-isItNumber(){
-check=`echo "$1" | grep -E ^\-?[0-9]*\.?[0-9]+$`
-if [ "$check" != '' ]; then
-    # It is an int or float
-    echo "$1"
-else
-    # Not a number, return -1 instead
-    echo "-1"
-fi
-}
+#isItNumber(){
+#check=`echo "$1" | grep -E ^\-?[0-9]*\.?[0-9]+$`
+#if [ "$check" != '' ]; then
+#    # It is an int or float
+#    echo "$1"
+#else
+#    # Not a number, return -1 instead
+#    echo "-1"
+#fi
+#}
+#
+#isItEmptyVar(){
+#    if [ -z "$1" ]; then
+#     # Empty string, return -1 instead
+#      echo "-1"
+#    else
+#    # Non-empty string
+#      echo "$1"
+#    fi
+#}
 
-isItEmptyVar(){
-    if [ -z "$1" ]; then
-     # Empty string, return -1 instead
-      echo "-1"
-    else
-    # Non-empty string
-      echo "$1"
-    fi
-}
-
+hostname=$(hostname)
 cpu_temp=$(( $(</sys/class/thermal/thermal_zone0/temp) / 1000 ))
 gpu_temp=$(vcgencmd measure_temp | cut -d"=" -f2 | cut -d"'" -f1)
 pmic_temp=$(vcgencmd measure_temp pmic | cut -d"'" -f1 | cut -d"=" -f2)
 cpu_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 cpu_volt=$(vcgencmd measure_volts | cut -d"=" -f2 | sed 's/V$//')
-cpu_throttled=$(vcgencmd get_throttled | cut -d"x" -f2)
-hostname=$(hostname)
-cpu_util=$(mpstat -o JSON | jq '(100-(.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle))')
+#Convert hex to int, keeping only the lower 4 bits.  Grafana translates the ints to text strings
+cpu_throttled=$(vcgencmd get_throttled | cut -d'x' -f2 | xargs -I% bash -c 'echo "$((16#% & 15))"')
+cpu_util=$(mpstat 1 1 -o JSON | jq '(100-(.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle))')
 mem_used=$(free | head -n2 | tail -n1 | awk '{print $3 / $2 * 100}')
 sdcard_used=$(df | grep "/dev/root" | awk '{print $5}' | cut -d"%" -f1)
-#gpu_util=0
-#sda1_used=$(df | grep "/dev/sda1" | awk '{print $5}' | cut -d"%" -f1)
-#sdb1_used=$(df | grep "/dev/sdb1" | awk '{print $5}' | cut -d"%" -f1)
 
-echo "pihealth,hostname=$hostname cpu_temp=$cpu_temp,gpu_temp=$gpu_temp,cpu_util=$cpu_util,mem_used=$mem_used,sdcard_used=$sdcard_used,pmic_temp=$pmic_temp,cpu_freq=$cpu_freq,cpu_volt=$cpu_volt,cpu_throttled=$cpu_throttled"
+#Return -1 if doesn't exist
+sda1_used=$(df | grep "/dev/sda1" | awk '{print $5}' | cut -d"%" -f1)
+if [ -z "$sda1_used"]; then sda1_used=-1; fi
+sdb1_used=$(df | grep "/dev/sdb1" | awk '{print $5}' | cut -d"%" -f1)
+if [ -z "$sdb1_used"]; then sdb1_used=-1; fi
+
+echo "pihealth,hostname=$hostname cpu_temp=$cpu_temp,gpu_temp=$gpu_temp,cpu_util=$cpu_util,mem_used=$mem_used,sdcard_used=$sdcard_used,pmic_temp=$pmic_temp,cpu_freq=$cpu_freq,cpu_volt=$cpu_volt,cpu_throttled=$cpu_throttled,sda1_used=$sda1_used,sdb1_used=$sdb1_used"
