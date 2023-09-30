e32d4dce15649bb6c7f22824419e2d6af86d3c4e
Bryan Ward <57335384+bryanward-net@users.noreply.github.com>
2023-09-29 20:58:11 -0400

    Make tografana.sh reusable

--- install/opt/wlanpi-grafana/health/stream-pihealth.sh
+++ install/opt/wlanpi-grafana/health/stream-pihealth.sh
@@ -2,4 +2,4 @@
 
 /opt/wlanpi-grafana/check-token.sh
 
-while true; do ./pihealth.sh | ./tografana.sh pihealth; sleep 60; done
+while true; do ./pihealth.sh | /opt/wlanpi-grafana/tografana.sh pihealth; sleep 60; done
