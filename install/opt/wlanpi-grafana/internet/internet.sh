497c56bb4cb1277f627f137ef3417e7330864759
Jiri Brejcha <jirka@jiribrejcha.net>
2023-10-02 05:01:00 -0500

    Improve Internet dashboard

--- install/opt/wlanpi-grafana/internet/internet.sh
+++ install/opt/wlanpi-grafana/internet/internet.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-hostname=$(hostname)
-#rtt=$(ping -c1 google.com | grep "rtt" | cut -d"/" -f5)
-speedtest_result=$(speedtest-cli --secure --csv)
-rtt=$(echo "$speedtest_result" | cut -d"," -f6)
-download=$(echo "$speedtest_result" | cut -d"," -f7)
-upload=$(echo "$speedtest_result" | cut -d"," -f8)
+default_gateway=$(ip route | grep "default" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -n1)
 
-echo "internet,hostname=$hostname rtt=$rtt,download=$download,upload=$upload"
+hostname=$(hostname)
+speedtest_result=$(speedtest-cli --secure --json)
+rtt_speedtest=$(echo "$speedtest_result" | jq -r ".ping")
+rtt_google=$(ping -c1 google.com | grep "rtt" | cut -d"/" -f5)
+rtt_gateway=$(ping -c1 "$default_gateway" | grep "rtt" | cut -d"/" -f5)
+download=$(echo "$speedtest_result" | jq -r ".download")
+upload=$(echo "$speedtest_result" | jq -r ".upload")
+
+echo "internet,hostname=$hostname rtt_speedtest=$rtt_speedtest,rtt_google=$rtt_google,rtt_gateway=$rtt_gateway,download=$download,upload=$upload"
