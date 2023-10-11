74cb9932b6c753929165a88422e5d2c9b6d2f28e
Bryan Ward <57335384+bryanward-net@users.noreply.github.com>
2023-10-11 09:20:22 -0400

    Refactor wispy

--- install/opt/wlanpi-grafana/wispy/wispy-5ghz.sh
+++ install/opt/wlanpi-grafana/wispy/wispy-5ghz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /opt/wlanpi-grafana/check-token.sh
 
-spectool_raw -r 2 2> /dev/null | spectool-client-parser > /dev/null
+spectool_raw -r 2 2> /dev/null | spectool-client-parser | /opt/wlanpi-grafana/to-grafana.sh wispy > /dev/null
